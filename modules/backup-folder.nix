acd577289cd0266c7d9fa3f7c20964d1124ec8cc
kirinnee <ernest@eng.atomi.cloud>
2022-02-02 16:52:14 +0800

    feat: better UI for backup system

--- modules/backup-folder.nix
+++ modules/backup-folder.nix
@@ -6,12 +6,27 @@ pkgs.writeShellScriptBin "pcloud-backup" ''
   target="$1"
   pcloud_folder="$2"
 
+
+  echo ⏳ Gathering timestamp and filename...
   timestamp="$(TZ='Singapore/Singapore' ${pkgs.coreutils}/bin/date '+%Y%b%d_%H%M')"
   name="archive-$timestamp.tar.gz"
+  echo ✅ "Timestamp: $timestamp; Filename: $name ;"
 
-  "${pkgs.gnutar}/bin/tar" -zcvf "$name" "$target"
 
-  "${pkgs.rclone}/bin/rclone" copy "./$name" "pcloud_remote:Backup/$pcloud_folder"
+  echo 📦 Begin packing...
+  "${pkgs.gnutar}/bin/tar" cf - "$target" | "${pkgs.pv}/bin/pv" -p -s "$(${pkgs.coreutils}/bin/du -sk "$target" | ${pkgs.coreutils}/bin/cut -f 1)k"  > "$name"
+  echo ✅ Done Packing!
 
-  rm "$name"
+  cleanup() {
+    echo 🗑️ Deleting archive...
+    rm "$name"
+    echo ✅ Completed deleting!
+  }
+  trap cleanup EXIT
+
+  echo 🚀 Uploading to pCloud using rclone...
+  "${pkgs.rclone}/bin/rclone" copy --progress "./$name" "pcloud_remote:Backup/$pcloud_folder"
+  echo ✅ Uploaded to pCloud!
+
+
 ''
