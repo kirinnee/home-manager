20eaefb9696d1fecfa937689c557d5df2dd54852
kirinnee <ernest@eng.atomi.cloud>
2022-02-09 13:57:04 +0800

    refact: cleaner module scripts

--- modules/backup-folder.nix
+++ modules/backup-folder.nix
@@ -1,4 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
+
+let script = builtins.readFile ./backup-folder.sh; in
 pkgs.writeShellScriptBin "pcloud-backup" ''
   #!/bin/sh
   set -e
@@ -11,32 +13,5 @@ pkgs.writeShellScriptBin "pcloud-backup" ''
   cut="${pkgs.coreutils}/bin/cut"
   rclone="${pkgs.rclone}/bin/rclone"
 
-  target="$1"
-  pcloud_folder="$2"
-
-
-  "$echo" ⏳ Gathering timestamp and filename...
-  timestamp="$(TZ='Singapore/Singapore' "$date" '+%Y%b%d_%H%M')"
-  name="archive-$timestamp.tar.gz"
-  "$echo" ✅ "Timestamp: $timestamp; Filename: $name ;"
-
-  cleanup() {
-    "$echo" 🗑️ Deleting archive...
-    rm "$name"
-    "$echo" ✅ Completed deleting!
-  }
-  trap cleanup EXIT
-
-
-  "$echo" 📦 Begin packing...
-  "$tar" cf - "$target" | "$pv" -p -s "$("$du" -sk "$target" | "$cut" -f 1)k"  > "$name"
-  "$echo" ✅ Done Packing!
-
-
-
-  "$echo" 🚀 Uploading to pCloud using rclone...
-  "$rclone" copy --progress "./$name" "pcloud_remote:Backup/$pcloud_folder"
-  "$echo" ✅ Uploaded to pCloud!
-
-
+  ${script}
 ''
