1604fcbb76f5a46aae90afdb4d3d8b65b46c69f1
kirinnee <ernest@eng.atomi.cloud>
2022-05-13 10:58:03 +0800

    fix: disable compfix

--- scripts/secon-user-setup-workspace.sh
+++ scripts/secon-user-setup-workspace.sh
@@ -8,6 +8,10 @@ git clone https://github.com/kirinnee/home-manager.git "$HOME/home-manager-confi
 
 cd "$HOME/.config" && rm -rf nixpkgs && ln -s "$HOME/home-manager-config" nixpkgs && cd ..
 
+rm "$HOME/home-manager-config/home.nix"
+
+mv "$HOME/home-manager-config/work.nix" "$HOME/home-manager-config/home.nix"
+
 bash --login -c "export NIXPKGS_ALLOW_UNFREE=1 && home-manager switch"
 
 bash --login -c "tmux new -d -s dockerd 'while true; do sudo $(which dockerd); done'"
