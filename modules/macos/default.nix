02a63eb95053baebc330ad83b1fbf1d82aa30553
ernest-liftoff <erng@liftoff.io>
2025-05-12 11:19:02 +0800

    feat(macos): remove httpie and zed
    - removed httpie from macos default.nix
    - removed httpie update script from update_mac.sh
    
    removing httpie and zed to streamline the macOS environment and reduce
    unnecessary dependencies.

--- modules/macos/default.nix
+++ modules/macos/default.nix
@@ -3,6 +3,5 @@ with nixpkgs;
 rec {
   firefox = import ./firefox/default.nix { inherit lib stdenvNoCC fetchurl undmg; };
   beekeeper-studio = import ./beekeeper-studio/default.nix { inherit lib stdenvNoCC fetchurl _7zz; xattr = darwin.xattr; };
-  httpie = import ./httpie/default.nix { inherit lib stdenvNoCC fetchurl undmg profile; };
   aptakube = import ./aptakube/default.nix { inherit lib stdenvNoCC fetchurl undmg profile; };
 }
--- scripts/update_mac.sh
+++ scripts/update_mac.sh
@@ -9,7 +9,6 @@ echo "✅ Done!"
 echo "⬆️ Updating all nix definitions..."
 ./scripts/updates/beekeeper-studio.sh &
 ./scripts/updates/aptakube.sh &
-./scripts/updates/httpie.sh &
 ./scripts/updates/firefox.sh &
 wait
 echo "✅ Done!"
