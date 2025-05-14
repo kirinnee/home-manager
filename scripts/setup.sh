90da3af0cb8ba67553d34e612f3716bdecff823c
kirinnee <kirinnee97@gmail.com>
2025-05-14 19:50:51 +0800

    fix(default): allow configuring age key
    - added a prompt for user to enter their age key
    - created a directory for storing age keys
    - saved the age key to a file with restricted permissions
    
    this change allows users to configure their age key securely, enhancing the
    setup process by ensuring that sensitive information is stored properly.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,18 +2,25 @@
 
 set -eou pipefail
 
-echo "⏬ Installing Nix..."
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
-echo "✅ Nix installation completed!"
+if command -v nix >/dev/null 2>&1; then
+  echo "🔍 Nix is found"
+else
+  echo "🔍  Nix is not installed"
+  echo "⏬ Installing Nix..."
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  echo "✅ Nix installation completed!"
+fi
 
 set +eou
 # shellcheck disable=SC1091
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 set -eou
 
-echo "⏬ Installing Home Manager..."
-nix run home-manager/release-23.11 -- init --switch
-echo "✅ Home Manager completed!"
+if [ ! -d "$HOME/.config/home-manager" ]; then
+  echo "⏬ Installing Home Manager..."
+  nix run home-manager/release-24.11 -- init --switch
+  echo "✅ Home Manager completed!"
+fi
 
 echo "🗑 Removing default configuration..."
 rm -rf ~/.config/home-manager
@@ -21,7 +28,16 @@ echo "✅ Remove existing Home Manager configuration!"
 
 echo "🔄 Syncing Home Manager..."
 nix run nixpkgs#git -- clone https://github.com/kirinnee/home-manager.git ~/.config/home-manager
-echo "✅ Synced Home Manager!"　
+echo "✅ Synced Home Manager!"
+
+# Ask for age key
+echo "❓ Enter your age key: "
+read -r age_key
+
+# Check if age key is valid
+mkdir -p ~/.config/sops/age
+echo "$age_key" >~/.config/sops/age/keys.txt
+chmod 0600 ~/.config/sops/age/keys.txt
 
 echo "🔥 Initialize Home Manager..."
 home-manager switch
