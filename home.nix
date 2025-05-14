542a9d9359da0c70aa4cdf8f826974eed9ace3b0
kirinnee <kirinnee97@gmail.com>
2025-05-14 19:06:28 +0800

    feat(home-manager): include secrets in home-manager
    - added .sops.yaml file
    - defined creation rules for secrets management
    
    this change introduces a new configuration file for managing secrets in
    home-manager, enhancing security and organization of sensitive data.

--- home.nix
+++ home.nix
@@ -1,5 +1,5 @@
-{ config, pkgs, pkgs-240924, pkgs-2411, pkgs-casks, atomi, profile, ... }:
+{ config, lib, pkgs, pkgs-240924, pkgs-2411, pkgs-casks, atomi, profile, ... }:
 
 import ./home-template.nix {
-  inherit config pkgs pkgs-240924 pkgs-2411 pkgs-casks atomi profile;
+  inherit config lib pkgs pkgs-240924 pkgs-2411 pkgs-casks atomi profile;
 }
--- modules/default.nix
+++ modules/default.nix
@@ -3,11 +3,7 @@ with nixpkgs;
 let trivialBuilders = import ./trivialBuilders.nix { inherit lib stdenv stdenvNoCC lndir runtimeShell shellcheck; }; in
 rec {
   backup-folder = import ./backup-folder/default.nix { inherit nixpkgs trivialBuilders; };
-  get-uuid = import ./get-uuid/default.nix { inherit nixpkgs trivialBuilders; };
-  register-with-github = import ./register-with-github/default.nix { inherit nixpkgs trivialBuilders get-uuid; };
-  set-signing-key = import ./set-signing-key/default.nix { inherit nixpkgs trivialBuilders; };
-  setup-devbox-server = import ./setup-devbox-server/default.nix { inherit nixpkgs trivialBuilders set-signing-key setup-keys register-with-github; };
-  setup-keys = import ./setup-keys/default.nix { inherit nixpkgs trivialBuilders; };
   setup-pcloud-remote = import ./setup-pcloud-remote/default.nix { inherit nixpkgs trivialBuilders; };
   k8s-update = import ./k8s-update/default.nix { inherit nixpkgs trivialBuilders; };
+  load-secrets = import ./load-secrets/default.nix { inherit nixpkgs trivialBuilders; };
 }
