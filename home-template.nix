ffc18f7adb7d71e729449d64346dacf2ad855264
kirinnee <kirinnee97@gmail.com>
2025-08-30 23:45:26 +0800

    feat(nix): remove atomi cache
    - removed the atomi cache URL from the list of substituters
    - removed the corresponding public key for the atomi cache
    
    the atomi cache is no longer needed, simplifying the configuration and
    potentially improving performance.

--- home-template.nix
+++ home-template.nix
@@ -55,7 +55,6 @@ rec {
         "https://cache.nixos.org?priority=41"
         "https://nix-community.cachix.org?priority=42"
         "https://numtide.cachix.org?priority=43"
-        "https://attic-atomicloud.fly.dev/all?priority=44"
       ];
 
       # Corresponding public keys for the substituters
@@ -63,7 +62,6 @@ rec {
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "all:uiWsynIsiSHKa2RuLZ3+8yE9lm1EVstE9RQ6RvRnVgU="
       ];
     };
     extraOptions = ''
